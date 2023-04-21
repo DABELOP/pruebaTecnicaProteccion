f2029b2bf124a8d47dfbb529aca640e13bc5ddd2
Danilo Bedoya <danilobedoya94@gmail.com>
2023-04-20 21:08:44 -0500

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/card.js'
+
 
 function App() {
+
+  const cardsNumber = 40
+  const cards = Array.from({ length:cardsNumber }, (_, i) => i+1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='contentWrapper'>
+
+      {cards.map(card => <Card show='img' id={card} key={card}/>)}
+
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+function Card(props) {
+
+    const [cardShow, setCardShow] = useState([props.show])
+    const [cardInfo, setCardInfo] = useState([])
+
+
+    useEffect(() => {
+        const link = 'https://www.superheroapi.com/api.php/10227755335285390/' + props.id
+        fetch(link)
+            .then(data => data.json())
+            .then(res => setCardInfo(res))
+            .catch(err => console.log(err))
+    }, [])
+
+
+    let showDetail = () =>{
+        cardShow[0] == 'detail' ? setCardShow(['img']) : setCardShow(['detail'])
+    }
+
+    return (
+
+        cardInfo == '' ? (<p>Cargando...</p>) : (
+
+            cardShow[0] == 'img' ? (
+                <div className='cardContainer' onClick={showDetail}>
+                    <img className='imgCard' src={cardInfo.image.url}></img>
+                </div>
+
+            ) : (
+
+                <div className='cardContainer' onClick={showDetail}>
+                    <h2 className='nombrePersonaje'>{cardInfo.name}</h2>
+
+                    <div className='nombreCompleto'>
+                        <p>Nombre completo:</p>
+                        <p>{cardInfo.biography['full-name']}</p>
+                    </div>
+
+                    <p>Genero: {cardInfo.appearance.gender}</p>
+
+                    <div className='primeraAparicion'>
+                        <p>first-appearance:</p>
+                        <p>{cardInfo.biography['first-appearance']}</p>
+                    </div>
+
+                </div>
+
+            )
+
+        )
+    )
+}
+
+export default Card 
